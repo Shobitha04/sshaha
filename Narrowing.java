00444d73487f7fe706b862383ee05cab1b1daa7c
M Shobitha <96161378+Shobitha04@users.noreply.github.com>
2023-09-17 13:06:16 +0530

    Create Narrowing.java

---
+++ Narrowing.java
@@ -0,0 +1,8 @@
+public class Narrowing {
+  public static void main(String[] args) {
+   double MyDoub=9.53453d;
+   int MyInt=(int)MyDoub;
+   System.out.println(MyDoub);
+   System.out.println(MyInt);
+  }
+}
