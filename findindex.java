10c0a34ac9372940051e9c8c3cc8d5283a72049a
M Shobitha <96161378+Shobitha04@users.noreply.github.com>
2023-09-17 13:18:48 +0530

    Create findindex.java

---
+++ findindex.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    String txt = "Harry Potter and Dumbledore make the best teacher student pair ever";
+    System.out.println(txt.indexOf("pair"));
+  }
+}
