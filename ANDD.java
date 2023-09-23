649b6d1342a97a92954dd9071be0e8c0444a1dc9
M Shobitha <96161378+Shobitha04@users.noreply.github.com>
2023-09-23 14:42:50 +0530

    Update ANDD.java

--- ANDD.java
+++ ANDD.java
@@ -1,6 +1,6 @@
 public class ANDD {
   public static void main(String[] args) {
-    int x = 5;
+    int x = 5;//initialising x variable with datatype integer
     System.out.println(x > 3 && x < 10); // returns true because 5 is greater than 3 AND 5 is less than 10
   }
 }
