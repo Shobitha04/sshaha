4b69b78a1e79bdd1736f68a21278a77b1f62e3a8
M Shobitha <96161378+Shobitha04@users.noreply.github.com>
2024-01-21 22:14:30 +0530

    Update Narrowing.java

--- Narrowing.java
+++ Narrowing.java
@@ -1,7 +1,7 @@
 public class Narrowing {
   public static void main(String[] args) {
    double MyDoub=9.53453d;
-   int MyInt=(int)MyDoub;
+   int MyInt=(int)MyDoub;//converting data types
    System.out.println(MyDoub);
    System.out.println(MyInt);
   }
